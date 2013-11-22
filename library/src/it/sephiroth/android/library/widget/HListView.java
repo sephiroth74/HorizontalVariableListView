bec2710d31552a315a5b56a6be9d3c6575015fbe
Alessandro Crugnola <alessandro@aviary.com>
2013-11-22 16:09:54 -0500

    maven release

--- library/src/it/sephiroth/android/library/widget/HListView.java
+++ library/src/it/sephiroth/android/library/widget/HListView.java
@@ -76,7 +76,7 @@ import android.widget.WrapperListAdapter;
 @RemoteView
 public class HListView extends AbsHListView {
 
-	public static final String VERSION = "1.0.0-SNAPSHOT";
+	public static final String VERSION = "1.0.0";
 
 	/**
 	 * Used to indicate a no preference for a position type.
